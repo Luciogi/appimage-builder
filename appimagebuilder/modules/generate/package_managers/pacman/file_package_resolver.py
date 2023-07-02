095d310b3d0c0e34b23a9a93fd54e4fba7e8399e
Luciogi <84625092+Luciogi@users.noreply.github.com>
2023-07-03 00:43:00 +0500

    fix pacman file query

--- appimagebuilder/modules/generate/package_managers/pacman/file_package_resolver.py
+++ appimagebuilder/modules/generate/package_managers/pacman/file_package_resolver.py
@@ -33,51 +33,22 @@ class FilePackageResolver:
         return results
 
     def _run_pacman_f(self, files):
-
-        argumentCharacters = len(" ".join(files))+20 # 20 lenght of string "/usr/bin/pacman -Qo "
-        print(argumentCharacters)
-        split = False
-        if argumentCharacters >= 65537:
-            split = True
-
         # make sure that the files are str
         files = [str(file) for file in files]
+        files = [f for f in files if not f.startswith('/home/')]
+
+        files = list(set(files))
+        command = ['pacman', '-F'] + files
 
         # ensure C locale is used to avoid locales affecting the output format
         env = os.environ.copy()
         env["LC_ALL"] = "C"
 
-        if split == False:
-            command = "{pacman} -Qo {files}"
-            command = command.format(**self._cli_tools, files=" ".join(files))
-            self.logger.info(command)
-            _proc = subprocess.run(command, stdout=subprocess.PIPE, shell=True, env=env)
-            stdout_data = _proc.stdout.decode()
-            return stdout_data
-        else:
-            # Calculate number of files then divide in half
-            TotalNumberOfFiles = len(files)
-            print(TotalNumberOfFiles)
-            if TotalNumberOfFiles%2 == 0:
-                totalNumberOfHalfFiles = int (TotalNumberOfFiles/2)
-            else:
-                totalNumberOfHalfFiles = int ((TotalNumberOfFiles-1)/2)
-            halfFiles1 = files[1:totalNumberOfHalfFiles]
-            halfFiles2 = files[totalNumberOfHalfFiles:TotalNumberOfFiles]
-            command1="{pacman} -Qo {halfFiles1}"
-            command2="{pacman} -Qo {halfFiles2}"
-
-            print(halfFiles2)
-            command1 = command1.format(**self._cli_tools, halfFiles1=" ".join(halfFiles1))
-            command2 = command2.format(**self._cli_tools, halfFiles2=" ".join(halfFiles2))
-            self.logger.info(command1)
-            _proc1 = subprocess.run(command1, stdout=subprocess.PIPE, shell=True, env=env)
-            self.logger.info(command2)
-            _proc2 = subprocess.run(command2, stdout=subprocess.PIPE, shell=True, env=env)
-            stdout_data1 = _proc1.stdout.decode()
-            stdout_data2 = _proc2.stdout.decode()
-            return stdout_data1
-
+        self.logger.info(command)
+        _proc = subprocess.run(
+            command, stdout=subprocess.PIPE, shell=False, env=env)
+        stdout_data = _proc.stdout.decode()
+        return stdout_data
 
     @staticmethod
     def _parse_pacman_f_output(output):
